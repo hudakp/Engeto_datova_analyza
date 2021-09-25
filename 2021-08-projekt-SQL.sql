e8bae7204c34bdcb9dd3d70bb614202749dfb397
Peter <78155934+hudakp@users.noreply.github.com>
2021-09-25 12:25:36 +0200

    Update 2021-08-projekt-SQL.sql

--- 2021-08-projekt-SQL.sql
+++ 2021-08-projekt-SQL.sql
@@ -27,12 +27,11 @@ SELECT
 		rel_buddhism.population / c_source.population AS 'Buddhism',
 		rel_folk_religions.population / c_source.population AS 'Folk_Religions',
 		rel_other_religions.population / c_source.population AS 'Other_Religions'
+		life_source.avg_life,
 		c_source.capital_city,
 		w_source.average_daily_temp,
 		w_source.rain_per_day,
 		w_source.gust_per_day,
-		life_source.avg_life,
-		
 FROM
 	covid19_tests ct
 LEFT JOIN (
